31232b6cddc951dbae90fae8cfa960e230af5321
Jason Chen Y <jason.y.chen@ericsson.com>
2015-04-23 23:19:03 +0200

    Adding json schema files and also code to parse them, external json jar is saved to git repository, here I'm using local IVY repository, actually can use the public IVY repository to download from internet directly, but worth a try.

---
+++ src/java/org/apache/nutch/companyschema/CompanySchema.java
@@ -0,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.companyschema;
+
+public class CompanySchema {
+    private String name;
+    private String url;
+    private String page_list_schema;
+    private String page_list_pattern;
+    private int page_list_increment;
+    private String page_list_last;
+    private String job_list_schema;
+    private String job_link;
+    private String job_title;
+    private String job_location;
+    private String job_date;
+
+    public CompanySchema(String n) { name = n; }
+    public String name() { return name; }
+    public void name(String n) { name = n; }
+
+    public String url() { return url; }
+    public void url(String u) { url = u; }
+
+    public String page_list_schema() { return page_list_schema; }
+    public void page_list_schema(String schema) { page_list_schema = schema; }
+
+    public String page_list_pattern() { return page_list_pattern; }
+    public void page_list_pattern(String pattern) { page_list_pattern = pattern; }
+
+    public int page_list_increment() { return page_list_increment; }
+    public void page_list_increment(int inc) { page_list_increment = inc; }
+
+    public String page_list_last() { return page_list_last; }
+    public void page_list_last(String last) { page_list_last = last; }
+
+    public String job_list_schema() { return job_list_schema; }
+    public void job_list_schema(String schema) { job_list_schema = schema; }
+
+    public String job_link() { return job_link; }
+    public void job_link(String link) { job_link = link; }
+
+    public String job_title() { return job_title; }
+    public void job_title(String title) { job_title = title; }
+
+    public String job_location() { return job_location; }
+    public void job_location(String loc) { job_location = loc; }
+
+    public String job_date() { return job_date; }
+    public void job_date(String date) { job_date = date; }
+
+    public void print(){
+        System.out.println("name:"+name);
+        System.out.println("url"+url);
+        System.out.println("page_list_schema:" + page_list_schema);
+        System.out.println("page_list_pattern:" + page_list_pattern);
+        System.out.println("page_list_incr:" + page_list_increment);
+        System.out.println("page_list_last:" + page_list_last);
+        System.out.println("job_list_schema:" + job_list_schema);
+        System.out.println("job_link:" + job_link);
+        System.out.println("job_title:" + job_title);
+        System.out.println("job_location:" + job_location);
+        System.out.println("job_date:" + job_date);
+    }
+}
+
---
+++ src/java/org/apache/nutch/companyschema/CompanySchemaRepository.java
@@ -0,0 +1,143 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.apache.nutch.companyschema;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.nutch.util.NutchConfiguration;
+
+import org.json.simple.parser.ContainerFactory;
+import org.json.simple.parser.ContentHandler;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.json.simple.JSONValue;
+import org.json.simple.JSONObject;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+public class CompanySchemaRepository {
+
+  private boolean auto;
+
+  private HashMap<String, CompanySchema> companies;
+
+  private Configuration conf;
+
+  public static final Logger LOG = LoggerFactory.getLogger(CompanySchemaRepository.class);
+
+  private JSONParser parser;
+
+  private String schemaDir;
+
+  public CompanySchemaRepository(Configuration conf) throws RuntimeException {
+    companies = new HashMap<String, CompanySchema>();
+    this.conf = new Configuration(conf);
+    this.auto = conf.getBoolean("company.schema.auto-load", false);
+    schemaDir = conf.get("company.schema.dir", "./");
+    parser = new JSONParser();
+  }
+
+  public CompanySchema getCompanySchema(String name) {
+    if (companies.containsKey(name))
+        return companies.get(name);
+
+    CompanySchema companySchema = parseCompanySchema(name);
+    if ( companySchema != null ) {
+        companies.put(name, companySchema);
+    }
+    return companySchema;
+  }
+
+  private CompanySchema parseCompanySchema(String name) {
+      FileReader fr = null;
+      try {
+          fr = new FileReader(schemaDir + "/" + name + ".json");
+      } catch (Exception e ) {
+          LOG.warn("File" + schemaDir + name + ".json not found");
+          return null;
+      };
+
+      parser.reset();
+      try {
+          JSONObject json = (JSONObject) parser.parse(fr);
+          fr.close();
+
+          if(!json.containsKey("company") || !name.equals((String)json.get("company")) ) {
+              LOG.warn("JSON file corruption or malformed: " + name);
+              return null;
+          }
+
+          CompanySchema companySchema = new CompanySchema(name);
+          companySchema.url((String) json.get("url"));
+
+          JSONObject page_list = (JSONObject)json.get("page_list");
+          companySchema.page_list_schema((String)page_list.get("schema"));
+          companySchema.page_list_pattern((String) page_list.get("pattern"));
+          companySchema.page_list_increment(Integer.parseInt((String)page_list.get("increment")));
+          companySchema.page_list_last((String)page_list.get("last"));
+
+          JSONObject page = (JSONObject)json.get("page");
+          companySchema.job_list_schema((String) page.get("schema"));
+          companySchema.job_link((String)page.get("job_link"));
+          companySchema.job_title((String)page.get("job_title"));
+          companySchema.job_location((String) page.get("job_location"));
+          companySchema.job_date((String)page.get("job_date"));
+
+          return companySchema;
+      } catch (Exception e) {
+          LOG.warn("JSON file parse failed: " + name);
+          //fr.close(); if parsing failed, fr will be collected by GC
+          return null;
+      }
+  }
+
+  private void displayStatus() {
+    LOG.info("Company Schema parsing mode: [" + this.auto + "]");
+    LOG.info("Parsed Company Schema:");
+
+    if ((companies == null) || (companies.size() == 0)) {
+      LOG.info("\tNONE");
+    } else {
+      for (CompanySchema company : companies.values()) {
+        LOG.info("\t" + company.name());
+      }
+    }
+  }
+
+  public static void main(String[] args) throws Exception {
+    if (args.length < 2) {
+      System.err
+          .println("Usage: CompanySchemaRepository name");
+      return;
+    }
+    Configuration conf = NutchConfiguration.create();
+    CompanySchemaRepository repo = new CompanySchemaRepository(conf);
+    // args[0] - company name
+    CompanySchema d = repo.getCompanySchema(args[0]);
+    if (d == null) {
+      System.err.println("Company Schema '" + args[0] + "' not present");
+      return;
+    }
+  }
+}
